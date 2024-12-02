7d896c440c3e4177980e5ab6b180941da4760ab7
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-12-02 17:13:35 +0700

    refactor : add sample data

---
+++ app/src/main/java/com/example/studentmanage/data/StudentList.kt
@@ -0,0 +1,12 @@
+package com.example.studentmanage.data
+
+import com.example.studentmanage.model.Student
+
+val students = mutableListOf(
+    Student("Nguyen Van A", "20225172"),
+    Student("Tran Thi B", "20225173"),
+    Student("Le Van C", "20225174"),
+    Student("Pham Thi D", "20225175"),
+    Student("Hoang Van E", "20225176"),
+    Student("Do Thi F", "20225177")
+)
